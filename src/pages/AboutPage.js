915c3aa5f386a0b8a712e63f6fd34890bcaf4d5e
evilbeliver <82883551+evilbeliver@users.noreply.github.com>
2023-09-04 21:42:17 -0400

    Updated Aboutus

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,4 +1,4 @@
-import { Col, Row, Container, Card, CardBody, CardHeader } from 'reactstrap';
+import { Col, Row, Container, Card, CardBody, } from 'reactstrap';
 import SubHeader from '../components/SubHeader';
 import PartnersList from '../features/parters/PartnersList';
 
@@ -8,51 +8,53 @@ const AboutPage = () => {
         <Container>
             <SubHeader current='About Us' />
             <Row className='row-content' >
-                <Col sm='6'>
-                    <h3>Our Mission</h3>
-                    <p> We present a curated database of the best campsites in the
-                        vast woods and backcountry of the World Wide Web Wilderness.
-                        We increase access to adventure for the public while
-                        promoting safe and respectful use of resources. The expert
-                        wilderness trekkers on our staff personally verify each
-                        campsite to make sure that they are up to our standards. We
-                        also present a platform for campers to share reviews on
-                        campsites they have visited with each other. 
+                <Col sm='11'>
+                    <h3>Our Story</h3>
+                    <p> At the conclusion of the 2012 deer hunting season, a group of men who had been hunting
+                            together for years met to discuss a vision of forming a new hunting club.  They collectively
+                            represented many years of hunting in very diverse clubs across the state and input from
+                            their experiences helped set the foundation for a new club.  By the end of the meeting, it
+                            was obvious that we all shared the same ideals for a FAMILY-ORIENTED hunting club and we
+                            stood united to pursue our vision.  On that day in January of 2013, the Buck & Beard Hunting
+                            Club became a reality and at that time, we adopted a simple, but powerful goal, where we
+                            “AIM” TO RESPECT THE SPORT, THE RESOURCES AND EACH OTHER!<br></br>
+
+                            Our membership can expect, in all our undertakings, SAFETY to be at the top of our list.  In
+                            all we do, before we act, we take every humanly conceivable risk out of the picture and
+                            through teamwork, we get the job done.
+                            We are a united group that takes great pride in our families and all participation they may
+                            have in our activities.  There is nothing quite like witnessing a youngster harvesting that
+                            first deer.  It is a milestone to be remembered for a lifetime and to be able to share in
+                            that moment brings joy to each of us.<br></br>
+
+                            Members are obligated to participate in work days, which typically begin in January.  While
+                            only 6 work days are required, most of our members come to as many work days as possible for
+                            various reasons: to learn the land; to take advantage of the cooler days in the early
+                            months; to get away from the typical, day-to-day norm;  to feel a sense of commitment to
+                            their club; and FELLOWSHIP.  When we close our work days, we may be sore and exhausted, but
+                            we feel good.  We have just spent quality time with friends and in the process, learned more
+                            about “my hunting club”.<br></br>
+
+                            We currently have large tracts of hunting property of mixed terrain in Saluda and Edgefield
+                            Counties of South Carolina.  Our annual membership roster is determined based on the acreage
+                            of hunting property we lease, the known and projected expenses, and the maintenance of a
+                            constant dues structure, while maintaining an excellent member to acre balance.  We do not
+                            solicit members for the sake of paying expenses.  We seek long-term members who will commit
+                            themselves to our ideals, become involved in helping us expand those ideals and get their
+                            families involved.
                     </p>
                 </Col>
-                <Col sm='6'>
-                    <Card>
-                        <CardHeader className='bg-primary text-white'>
-                            <h3>Facts at a Glance</h3>
-                        </CardHeader>
-                            <CardBody>
-                                <dl className='row'>
-                                    <dt className='col-6'>Founded</dt>
-                                    <dd className='col-6'>February 3, 2016</dd>
-                                    <dt className='col-6'>No. of Campsites in 2019</dt>
-                                    <dd className='col-6'>563</dd>
-                                    <dt className='col-6'>No. of Reviews in 2019</dt>
-                                    <dd className='col-6'>4388</dd>
-                                    <dt className='col-6'>Employees</dt>
-                                    <dd className='col-6'>42</dd>
-                                </dl>
-                            </CardBody>
-                    </Card>
-                </Col>
+ 
                 <Col>
                     <Card className='bg-light mt-3'>
                         <CardBody >
                             <blockquote className='blockquote'>
-                                <p>I will not follow where the path may lead, but I
-                                        will go where there is no path, and I will leave
-                                        a trail.
+                                <p> “A hunt based only on trophies taken falls far short of what the ultimate goal should be.” 
+
+
                                 </p>
                                 <footer className='blockquote-footer'> 
-                                        Muriel Strode,{' '}
-                                        <cite title='Source Title'>
-                                            "Wind-Wafted Wild Flowers" - The Open Court,
-                                            1903
-                                        </cite>
+                                        Fred Bear{' '}
                                 </footer>
                             </blockquote>
                         </CardBody>
