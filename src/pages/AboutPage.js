19ef06afe3d88970a971e412d1da68ed97ce6208
evilbeliver <82883551+evilbeliver@users.noreply.github.com>
2023-09-05 10:34:35 -0400

    added spacing

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -9,7 +9,7 @@ const AboutPage = () => {
             <SubHeader current='About Us' />
             <Row className='row-content' >
                 <Col sm='11'>
-                    <p> At the conclusion of the 2012 deer hunting season, a group of men who had been hunting
+                <p> At the conclusion of the 2012 deer hunting season, a group of men who had been hunting
                             together for years met to discuss a vision of forming a new hunting club.  They collectively
                             represented many years of hunting in very diverse clubs across the state and input from
                             their experiences helped set the foundation for a new club.  By the end of the meeting, it
@@ -17,7 +17,8 @@ const AboutPage = () => {
                             stood united to pursue our vision.  On that day in January of 2013, the BuckandBeard Hunting
                             Club became a reality and at that time, we adopted a simple, but powerful goal, where we
                             “AIM” TO RESPECT THE SPORT, THE RESOURCES AND EACH OTHER!
-
+                    </p>
+                        <p>
                             Our membership can expect, in all our undertakings, SAFETY to be at the top of our list.  In
                             all we do, before we act, we take every humanly conceivable risk out of the picture and
                             through teamwork, we get the job done.
@@ -25,7 +26,7 @@ const AboutPage = () => {
                             have in our activities.  There is nothing quite like witnessing a youngster harvesting that
                             first deer.  It is a milestone to be remembered for a lifetime and to be able to share in
                             that moment brings joy to each of us.
-
+                        </p>
                             Members are obligated to participate in work days, which typically begin in January.  While
                             only 6 work days are required, most of our members come to as many work days as possible for
                             various reasons: to learn the land; to take advantage of the cooler days in the early
@@ -33,7 +34,7 @@ const AboutPage = () => {
                             their club; and FELLOWSHIP.  When we close our work days, we may be sore and exhausted, but
                             we feel good.  We have just spent quality time with friends and in the process, learned more
                             about “my hunting club”.
-
+                        <p>
                             We currently have large tracts of hunting property of mixed terrain in Saluda and Edgefield
                             Counties of South Carolina.  Our annual membership roster is determined based on the acreage
                             of hunting property we lease, the known and projected expenses, and the maintenance of a
--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -17,7 +17,8 @@ const FAQpage = () => {
                             stood united to pursue our vision.  On that day in January of 2013, the BuckandBeard Hunting
                             Club became a reality and at that time, we adopted a simple, but powerful goal, where we
                             “AIM” TO RESPECT THE SPORT, THE RESOURCES AND EACH OTHER!
-
+                    </p>
+                        <p>
                             Our membership can expect, in all our undertakings, SAFETY to be at the top of our list.  In
                             all we do, before we act, we take every humanly conceivable risk out of the picture and
                             through teamwork, we get the job done.
@@ -25,7 +26,7 @@ const FAQpage = () => {
                             have in our activities.  There is nothing quite like witnessing a youngster harvesting that
                             first deer.  It is a milestone to be remembered for a lifetime and to be able to share in
                             that moment brings joy to each of us.
-
+                        </p>
                             Members are obligated to participate in work days, which typically begin in January.  While
                             only 6 work days are required, most of our members come to as many work days as possible for
                             various reasons: to learn the land; to take advantage of the cooler days in the early
@@ -33,7 +34,7 @@ const FAQpage = () => {
                             their club; and FELLOWSHIP.  When we close our work days, we may be sore and exhausted, but
                             we feel good.  We have just spent quality time with friends and in the process, learned more
                             about “my hunting club”.
-
+                        <p>
                             We currently have large tracts of hunting property of mixed terrain in Saluda and Edgefield
                             Counties of South Carolina.  Our annual membership roster is determined based on the acreage
                             of hunting property we lease, the known and projected expenses, and the maintenance of a
