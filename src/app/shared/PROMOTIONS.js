d39dae3f6abfd7d80332e8fa41062735267f069a
evilbeliver <82883551+evilbeliver@users.noreply.github.com>
2023-12-15 16:48:50 -0500

    Cleaning up Code

--- src/app/shared/PROMOTIONS.js
+++ src/app/shared/PROMOTIONS.js
@@ -1,7 +1,7 @@
 import turkeyImg from '/Users/davidcristinzio/Desktop/BuckandBeardReact/src/app/assets/img/turkey.jpg';
-import cfBuck3Img from '/Users/davidcristinzio/Desktop/BuckandBeardReact/src/app/assets/img/CF Buck 3.jpeg';
-import cfBuckImg from '/Users/davidcristinzio/Desktop/BuckandBeardReact/src/app/assets/img/CF Buck.jpeg';
-import cloverImg from '/Users/davidcristinzio/Desktop/BuckandBeardReact/src/app/assets/img/Clover.jpeg'
+//import cfBuck3Img from '/Users/davidcristinzio/Desktop/BuckandBeardReact/src/app/assets/img/CF Buck 3.jpeg';
+//import cfBuckImg from '/Users/davidcristinzio/Desktop/BuckandBeardReact/src/app/assets/img/CF Buck.jpeg';
+//import cloverImg from '/Users/davidcristinzio/Desktop/BuckandBeardReact/src/app/assets/img/Clover.jpeg'
 
 
 export const PROMOTIONS = [
@@ -15,27 +15,27 @@ export const PROMOTIONS = [
         description:
             'Our properties are not only managed for deer but for some great spring turkey hunting as well.'
     },
-    {
-        id: 1,
-        name: 'Into The Weeds',
-        image: cfBuck3Img,
-        featured: true,
-        description:
-            '5 days deep in the Redux National Monument, far from the beaten path, with only a reducer in your backpack. Guide provided.'
-    },
-    {
-        id: 2,
-        name: 'Bootstrap Teardown',
-        image: cfBuckImg,
-        featured: true,
-        description: `In this weekend workshop, we'll rebuild your bootstraps from scratch, so you're ready for next-level excursions.`
-    },
-    {
-        id: 3,
-        name: 'Asynchronous Rendezvous',
-        image: cloverImg,
-        featured: false,
-        description:
-            'Meet up with your guide at the top of Promise Mountain. Or, try to. Good luck!'
-    }
+   // {
+     //   id: 1,
+      //  name: 'Into The Weeds',
+      //  image: cfBuck3Img,
+      //  featured: true,
+       // description:
+       //     '5 days deep in the Redux National Monument, far from the beaten path, with only a reducer in your backpack. Guide provided.'
+   // },
+  //  {
+  //      id: 2,
+  //      name: 'Bootstrap Teardown',
+   //     image: cfBuckImg,
+    //    featured: true,
+      //  description: `In this weekend workshop, we'll rebuild your bootstraps from scratch, so you're ready for next-level excursions.`
+    //},
+   // {
+    //    id: 3,
+    //    name: 'Asynchronous Rendezvous',
+    //    image: cloverImg,
+    //    featured: false,
+    //    description:
+    //        'Meet up with your guide at the top of Promise Mountain. Or, try to. Good luck!'
+ //  }
 ];
